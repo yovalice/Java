36c64322ac6d2add7d067ff6e88d583eae93e072
Yovalice Roman <yovalice@me.com>
2017-04-19 22:52:36 +0800

    Adding some comments and overloading a method

--- Review/src/main/java/com/javafundamentals/model/PolymorphismCarModel.java
+++ Review/src/main/java/com/javafundamentals/model/PolymorphismCarModel.java
@@ -4,20 +4,23 @@ package com.javafundamentals.model;
  * Created by yovaliceroman on 4/19/17.
  */
 
-//class that extends PolymorphismVehicleModel and override again the method
-//from the vehicleInterface
+//class that extends PolymorphismVehicleModel and override the method
+//from the vehicleInterface. It shows constructor and method overloading
+//and the use of super
 public class PolymorphismCarModel extends PolymorphismVehicleModel {
 
     //private instance variables
     private boolean hasDoor;
     private int doorNum;
 
+    //examples of constructor overloading
     //constructor with no parameter specify
     public PolymorphismCarModel(){
 
     }
 
-    //constructor with parameter specify
+    //constructor with parameters specify
+    //use of super keyword
     public PolymorphismCarModel(int year, int wheels, String brand, boolean hasDoor, int doorNum){
         super(year, wheels, brand);
         this.hasDoor = hasDoor;
@@ -29,6 +32,7 @@ public class PolymorphismCarModel extends PolymorphismVehicleModel {
     public double costPerMile() {
         double costPerMile;
 
+        //use of super
         if (super.getWheels() < 3){
             costPerMile = 8.75;
             System.out.println("Cost per mile is 5.0");
@@ -38,6 +42,14 @@ public class PolymorphismCarModel extends PolymorphismVehicleModel {
         }
 
         return costPerMile;
+    }
+
+    //method overloading example
+    //it takes an int as a parameter
+    public double costPerMile(int costPerMileEx){
+        costPerMileEx = 9;
+
+        return costPerMileEx;
     }
 
 }
